f5c59d84e4dbf698c02a150e4bcdb9119b2fbfc0
Nathalia Maia <devmaia@outlook.com>
2023-11-03 00:48:08 -0300

    🎨 UI: Updated navigation, footer, Home and Blog layout.

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure the database context
 builder.Configuration.AddJsonFile("appsettings.json", optional: false);
 builder.Configuration.AddEnvironmentVariables();
 
@@ -15,16 +14,13 @@ builder.Services.AddDbContext<BlogContext>(options =>
     options.UseSqlServer(connectionString);
 });
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -37,6 +33,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Blog}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
